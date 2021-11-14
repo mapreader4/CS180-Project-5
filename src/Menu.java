82dd4e29defa57b0278cb47b5caa4a7f06d0a9a1
mapreader4 <mapreader4@gmail.com>
2021-11-14 16:43:54 -0500

    Updated Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -318,7 +318,7 @@ public class Menu {
                 course.setCourseName(courseName);
             }
         }
-        courseList.update(course);
+        courseList.updateCourses(course);
     }
 
     /**
@@ -429,7 +429,7 @@ public class Menu {
                 submission.view(scanner);
             }
         }
-        courseList.update(course);
+        courseList.updateCourses(course);
     }
 
     /**
