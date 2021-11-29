95cdd2240a709135fcc298df9a87cd72e9856aa1
mapreader4 <mapreader4@gmail.com>
2021-11-29 18:07:12 -0500

    added View.java

---
+++ src/View.java
@@ -0,0 +1,12 @@
+/**
+ * Online Quiz Navigator v2 - View
+ *
+ * Handles the GUI for the Online Quiz Navigator.
+ * Listens for user updates, then sends them to Client for handling.
+ * Listens for updates from Client, then refreshes the board as need be.
+ *
+ * @author Nathan Reed, lab sec L24
+ * @version November 29, 2021
+ */
+public class View {
+}
