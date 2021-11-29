e392c975fd59b395e2b6f9e98589230a5470198b
mapreader4 <mapreader4@gmail.com>
2021-11-29 18:47:47 -0500

    made constructor for View.java

--- src/View.java
+++ src/View.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 /**
  * Online Quiz Navigator v2 - View
  *
@@ -8,5 +12,30 @@
  * @author Nathan Reed, lab sec L24
  * @version November 29, 2021
  */
-public class View {
+public class View extends Container {
+    Client client;
+
+    /**
+     * Sets up View object and Event Dispatch Thread when called by Client
+     * Structure borrowed from Week 11 Wednesday lecture slide 75
+     *
+     * @param client the Client object used for this session
+     */
+    View(Client client) {
+        this.client = client;
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createGUI();
+            }
+        });
+    }
+
+    private void createGUI() {
+
+    }
+
+    private void paint() {
+
+    }
 }
