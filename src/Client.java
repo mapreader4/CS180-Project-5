aa4281b5e1dd86cdacc979eb12637f560e302e81
jaysmehta208 <jaysmehta@gmail.com>
2021-12-05 17:44:36 -0500

    Fixed connectToServer method in client

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,10 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
+    Socket socket=null;
+    PrintWriter pw=null;
+    BufferedReader bfr=null;
     public static void main(String[] args) {
         Client client=new Client();
         View view=new View(client);
@@ -10,12 +13,26 @@ public class Client {
     public boolean connectToServer(String domainName, String inputPortNumber) {
         try {
             int portNumber = Integer.parseInt(inputPortNumber);
-            Socket socket = new Socket(domainName, portNumber);
+            socket = new Socket(domainName, portNumber);
             if(socket.isConnected()){
                 return true;
             }
+            pw=new PrintWriter(socket.getOutputStream());
+            bfr=new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (Exception e) {
             return false;
+        }
+        return false;
+    }
+    public boolean createAccount(String username, String password, String typeOfAccount){
+        pw.println("login "+username+" "+ password + " "+typeOfAccount);
+        try {
+            String checker = bfr.readLine();
+            if(checker=="success"){
+                return true;
+            }
+        } catch (IOException e){
+            throw new RuntimeException();
         }
         return false;
     }
